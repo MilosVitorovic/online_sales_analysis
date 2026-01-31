da512d843b08638dbaa24ff2f12531a5b0dbe907
Miloš Vitorović <milosvitorovic1997@gmail.com>
2026-01-31 22:36:53 +0100

    Initial project structure with Product and ProductManager

---
+++ main.py
@@ -0,0 +1,15 @@
+from product import Product
+from product_manager import ProductManager
+
+manager = ProductManager()
+
+p1 = Product("Laptop", 1000, 3)
+p2 = Product("Miš", 20, 10)
+p3 = Product("Tastatura", 50, 5)
+
+manager.add_product(p1)
+manager.add_product(p2)
+manager.add_product(p3)
+
+manager.show_products()
+print("Ukupna vrednost inventara:", manager.total_inventory_value())
