161b179bb70ad72dda56426d221575049a429d1e
Miloš Vitorović <milosvitorovic1997@gmail.com>
2026-01-31 23:01:32 +0100

    Add Cart class and shopping cart functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
 
 manager = ProductManager()
 
@@ -11,5 +12,11 @@ manager.add_product(p1)
 manager.add_product(p2)
 manager.add_product(p3)
 
-manager.show_products()
-print("Ukupna vrednost inventara:", manager.total_inventory_value())
+cart = Cart()
+
+cart.add_to_cart(p1)
+cart.add_to_cart(p2)
+cart.add_to_cart(p3)
+
+cart.show_cart()
+print("Ukupno za naplatu:", cart.total_cart_value())
